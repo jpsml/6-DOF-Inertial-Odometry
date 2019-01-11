96037a3b69a70595fcff07414eeea4767a57d71d
Joao Paulo Lima <jpsml@cin.ufpe.br>
2019-01-11 14:57:05 +0900

    change to use CuDNNLSTM

--- model.py
+++ model.py
@@ -1,16 +1,13 @@
 from keras.models import Sequential
-from keras.layers import Bidirectional, LSTM, Dropout, Dense
+from keras.layers import Bidirectional, CuDNNLSTM, Dropout, Dense
 from keras.optimizers import Adam
 
 def create_model(window_size = 200):
 
     model = Sequential()
 
-    #model.add(Bidirectional(LSTM(96, return_sequences=True), input_shape=(window_size, 6)))
-    #model.add(Bidirectional(LSTM(96)))
-
-    model.add(LSTM(96, input_shape=(window_size, 6)))
-
+    model.add(Bidirectional(CuDNNLSTM(96, return_sequences=True), input_shape=(window_size, 6)))
+    model.add(Bidirectional(CuDNNLSTM(96)))
     model.add(Dropout(0.25))
     model.add(Dense(2))
 
