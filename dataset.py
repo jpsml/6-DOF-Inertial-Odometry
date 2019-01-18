462a3ee7d762ea3f520663dd9522c7fdf0b6baba
Joao Paulo Lima <jpsml@cin.ufpe.br>
2019-01-18 10:54:03 +0900

    extend to 3D case

--- dataset.py
+++ dataset.py
@@ -1,7 +1,75 @@
 import numpy as np
 from keras.utils import Sequence
 
-def load_dataset(imu_data_filename, gt_data_filename, window_size=200, stride=10):
+def cartesian_to_spherical_coordinates(point_cartesian):
+    delta_l = np.linalg.norm(point_cartesian)
+
+    if np.absolute(delta_l) > 1e-05:
+        theta = np.arccos(point_cartesian[2] / delta_l)
+        psi = np.arctan2(point_cartesian[1], point_cartesian[0])
+        return delta_l, theta, psi
+    else:
+        return 0, 0, 0
+
+def load_dataset_3d(imu_data_filename, gt_data_filename, window_size=200, stride=10):
+
+    imu_data = np.genfromtxt(imu_data_filename, delimiter=',')
+    gt_data = np.genfromtxt(gt_data_filename, delimiter=',')
+
+    imu_data = imu_data[1200:-300]
+    gt_data = gt_data[1200:-300]
+    
+    gyro_acc_data = np.concatenate([imu_data[:, 4:7], imu_data[:, 10:13]], axis=1)
+    
+    loc_data = gt_data[:, 2:5]
+
+    l0 = loc_data[window_size//2 - stride//2 - stride, :]
+    l1 = loc_data[window_size//2 - stride//2, :]
+    init_l = l1
+    delta_l, init_theta, init_psi = cartesian_to_spherical_coordinates(l1 - l0)
+
+    x = []
+    y_delta_l = []
+    y_delta_theta = []
+    y_delta_psi = []
+
+    for idx in range(0, gyro_acc_data.shape[0] - window_size - 1, stride):
+        x.append(gyro_acc_data[idx + 1 : idx + 1 + window_size, :])
+
+        delta_l0, theta0, psi0 = cartesian_to_spherical_coordinates(loc_data[idx + window_size//2 - stride//2, :] - loc_data[idx + window_size//2 - stride//2 - stride, :])
+
+        l0 = loc_data[idx + window_size//2 - stride//2, :]
+        l1 = loc_data[idx + window_size//2 + stride//2, :]
+
+        delta_l, theta1, psi1 = cartesian_to_spherical_coordinates(l1 - l0)
+
+        delta_theta = theta1 - theta0
+        delta_psi = psi1 - psi0
+
+        if delta_theta < -np.pi:
+            delta_theta += 2 * np.pi
+        elif delta_theta > np.pi:
+            delta_theta -= 2 * np.pi
+
+        if delta_psi < -np.pi:
+            delta_psi += 2 * np.pi
+        elif delta_psi > np.pi:
+            delta_psi -= 2 * np.pi
+
+        y_delta_l.append(np.array([delta_l]))
+        y_delta_theta.append(np.array([delta_theta]))
+        y_delta_psi.append(np.array([delta_psi]))
+
+
+    x = np.reshape(x, (len(x), x[0].shape[0], x[0].shape[1]))
+    y_delta_l = np.reshape(y_delta_l, (len(y_delta_l), y_delta_l[0].shape[0]))
+    y_delta_theta = np.reshape(y_delta_theta, (len(y_delta_theta), y_delta_theta[0].shape[0]))
+    y_delta_psi = np.reshape(y_delta_psi, (len(y_delta_psi), y_delta_psi[0].shape[0]))
+
+    return x, [y_delta_l, y_delta_theta, y_delta_psi], init_l, init_theta, init_psi
+
+
+def load_dataset_2d(imu_data_filename, gt_data_filename, window_size=200, stride=10):
 
     imu_data = np.genfromtxt(imu_data_filename, delimiter=',')
     gt_data = np.genfromtxt(gt_data_filename, delimiter=',')
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from mpl_toolkits.mplot3d import Axes3D
+
 from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 
@@ -9,7 +11,24 @@ from sklearn.utils import shuffle
 from dataset import *
 from model import *
 
-def generate_trajectory(init_l, init_psi, y_delta_l, y_delta_psi):
+def generate_trajectory_3d(init_l, init_theta, init_psi, y_delta_l, y_delta_theta, y_delta_psi):
+    cur_l = np.array(init_l)
+    cur_theta = np.array(init_theta)
+    cur_psi = np.array(init_psi)
+    pred_l = []
+    pred_l.append(np.array(cur_l))
+
+    for [delta_l, delta_theta, delta_psi] in zip(y_delta_l, y_delta_theta, y_delta_psi):
+        cur_theta = cur_theta + delta_theta
+        cur_psi = cur_psi + delta_psi
+        cur_l[0] = cur_l[0] + delta_l * np.sin(cur_theta) * np.cos(cur_psi)
+        cur_l[1] = cur_l[1] + delta_l * np.sin(cur_theta) * np.sin(cur_psi)
+        cur_l[2] = cur_l[2] + delta_l * np.cos(cur_theta)
+        pred_l.append(np.array(cur_l))
+
+    return np.reshape(pred_l, (len(pred_l), 3))
+
+def generate_trajectory_2d(init_l, init_psi, y_delta_l, y_delta_psi):
 	cur_l = np.array(init_l)
 	cur_psi = np.array(init_psi)
 	pred_l = []
@@ -31,6 +50,7 @@ stride = 10
 
 x = []
 y_delta_l = []
+y_delta_theta = []
 y_delta_psi = []
 
 imu_data_filenames = []
@@ -87,12 +107,19 @@ gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/vi
 gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/vi4.csv')
 
 for i, (cur_imu_data_filename, cur_gt_data_filename) in enumerate(zip(imu_data_filenames, gt_data_filenames)):
-    cur_x, [cur_y_delta_l, cur_y_delta_psi], init_l, init_psi = load_dataset(cur_imu_data_filename, cur_gt_data_filename, window_size, stride)
+    #cur_x, [cur_y_delta_l, cur_y_delta_psi], init_l, init_psi = load_dataset_2d(cur_imu_data_filename, cur_gt_data_filename, window_size, stride)
+    cur_x, [cur_y_delta_l, cur_y_delta_theta, cur_y_delta_psi], init_l, init_theta, init_psi = load_dataset_3d(cur_imu_data_filename, cur_gt_data_filename, window_size, stride)
 
     #plt.figure()
     #plt.plot(cur_y_delta_l)
     #plt.title('Delta L ' + str(i))
     #plt.ylabel('m')
+    #plt.xlabel('time (0.1s)')
+
+    #plt.figure()
+    #plt.plot(cur_y_delta_theta)
+    #plt.title('Delta Theta ' + str(i))
+    #plt.ylabel('rad')
     #plt.xlabel('time (0.1s)')
 
     #plt.figure()
@@ -105,43 +132,43 @@ for i, (cur_imu_data_filename, cur_gt_data_filename) in enumerate(zip(imu_data_f
 
     x.append(cur_x)
     y_delta_l.append(cur_y_delta_l)
+    y_delta_theta.append(cur_y_delta_theta)
     y_delta_psi.append(cur_y_delta_psi)
 
 x = np.vstack(x)
 y_delta_l = np.vstack(y_delta_l)
+y_delta_theta = np.vstack(y_delta_theta)
 y_delta_psi = np.vstack(y_delta_psi)
 
-x, y_delta_l, y_delta_psi = shuffle(x, y_delta_l, y_delta_psi)
+#x, y_delta_l, y_delta_psi = shuffle(x, y_delta_l, y_delta_psi)
+x, y_delta_l, y_delta_theta, y_delta_psi = shuffle(x, y_delta_l, y_delta_theta, y_delta_psi)
 
 do_training = True
 
 if do_training:
-	model = create_model(window_size)
+    #model = create_model_2d(window_size)
+    model = create_model_3d(window_size)
 
-	#model_checkpoint = ModelCheckpoint('bidirectional_lstm.hdf5', monitor='loss', save_best_only=True, verbose=1)
-	model_checkpoint = ModelCheckpoint('bidirectional_lstm.hdf5', monitor='val_loss', save_best_only=True, verbose=1)
+    model_checkpoint = ModelCheckpoint('bidirectional_lstm.hdf5', monitor='val_loss', save_best_only=True, verbose=1)
 
-	history = model.fit(x, [y_delta_l, y_delta_psi], epochs=400, batch_size=512, verbose=1, callbacks=[model_checkpoint], validation_split=0.1)
+    #history = model.fit(x, [y_delta_l, y_delta_psi], epochs=400, batch_size=512, verbose=1, callbacks=[model_checkpoint], validation_split=0.1)
+    history = model.fit(x, [y_delta_l, y_delta_theta, y_delta_psi], epochs=400, batch_size=512, verbose=1, callbacks=[model_checkpoint], validation_split=0.1)
 
-	#plt.plot(history.history['loss'])
-	#plt.title('Model loss')
-	#plt.ylabel('Loss')
-	#plt.xlabel('Epoch')
-	#plt.show()
-
-	plt.plot(history.history['loss'])
-	plt.plot(history.history['val_loss'])
-	plt.title('Model loss')
-	plt.ylabel('Loss')
-	plt.xlabel('Epoch')
-	plt.legend(['Train', 'Validation'], loc='upper left')
-	plt.show()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
 
 model = load_model('bidirectional_lstm.hdf5')
 
-x, [y_delta_l, y_delta_psi], init_l, init_psi = load_dataset('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu1.csv', 'Oxford Inertial Tracking Dataset/multi users/user2/syn/vi1.csv', window_size, stride)
+#x, [y_delta_l, y_delta_psi], init_l, init_psi = load_dataset_2d('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu1.csv', 'Oxford Inertial Tracking Dataset/multi users/user2/syn/vi1.csv', window_size, stride)
+x, [y_delta_l, y_delta_theta, y_delta_psi], init_l, init_theta, init_psi = load_dataset_3d('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu1.csv', 'Oxford Inertial Tracking Dataset/multi users/user2/syn/vi1.csv', window_size, stride)
 
-[yhat_delta_l, yhat_delta_psi] = model.predict(x, batch_size=1, verbose=1)
+#[yhat_delta_l, yhat_delta_psi] = model.predict(x, batch_size=1, verbose=1)
+[yhat_delta_l, yhat_delta_theta, yhat_delta_psi] = model.predict(x, batch_size=1, verbose=1)
 
 plt.figure()
 plt.plot(y_delta_l)
@@ -152,54 +179,95 @@ plt.xlabel('Time (0.1s)')
 plt.legend(['Delta L Ground Truth', 'Delta L Pred'], loc='upper left')
 
 plt.figure()
+plt.plot(y_delta_theta)
+plt.plot(yhat_delta_theta)
+plt.title('Delta Theta Pred vs Ground Truth')
+plt.ylabel('Delta Theta (rad)')
+plt.xlabel('Time (0.1s)')
+plt.legend(['Delta Theta Ground Truth', 'Delta Theta Pred'], loc='upper left')
+
+plt.figure()
 plt.plot(y_delta_psi)
 plt.plot(yhat_delta_psi)
 plt.title('Delta Psi Pred vs Ground Truth')
 plt.ylabel('Delta Psi (rad)')
 plt.xlabel('Time (0.1s)')
 plt.legend(['Delta Psi Ground Truth', 'Delta Psi Pred'], loc='upper left')
-plt.show()
-
-gt_trajectory = generate_trajectory(init_l, init_psi, y_delta_l, y_delta_psi)
-pred_trajectory = generate_trajectory(init_l, init_psi, yhat_delta_l, yhat_delta_psi)
-pred_trajectory_only_l = generate_trajectory(init_l, init_psi, yhat_delta_l, y_delta_psi)
-pred_trajectory_only_psi = generate_trajectory(init_l, init_psi, y_delta_l, yhat_delta_psi)
-
-#gt_trajectory = generate_trajectory(init_l, init_psi, y_delta_l[::20], y_delta_psi[::20])
-#pred_trajectory = generate_trajectory(init_l, init_psi, yhat_delta_l[::20], yhat_delta_psi[::20])
-#pred_trajectory_only_l = generate_trajectory(init_l, init_psi, yhat_delta_l[::20], y_delta_psi[::20])
-#pred_trajectory_only_psi = generate_trajectory(init_l, init_psi, y_delta_l[::20], yhat_delta_psi[::20])
-
-plt.figure()
-#plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
-#plt.plot(pred_trajectory[:, 0], pred_trajectory[:, 1])
-plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
-plt.plot(pred_trajectory[0:200, 0], pred_trajectory[0:200, 1])
-plt.title('Trajectory Pred vs Ground Truth')
-plt.ylabel('Y (m)')
-plt.xlabel('X (m)')
-plt.legend(['Trajectory Ground Truth', 'Trajectory Pred'], loc='upper left')
-
-plt.figure()
-#plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
-#plt.plot(pred_trajectory_only_l[:, 0], pred_trajectory_only_l[:, 1])
-plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
-plt.plot(pred_trajectory_only_l[0:200, 0], pred_trajectory_only_l[0:200, 1])
-plt.title('Trajectory Pred Only Delta L vs Ground Truth')
-plt.ylabel('Y (m)')
-plt.xlabel('X (m)')
-plt.legend(['Trajectory Ground Truth', 'Trajectory Pred Only Delta Psi'], loc='upper left')
-
-plt.figure()
-#plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
-#plt.plot(pred_trajectory_only_psi[:, 0], pred_trajectory_only_psi[:, 1])
-plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
-plt.plot(pred_trajectory_only_psi[0:200, 0], pred_trajectory_only_psi[0:200, 1])
-plt.title('Trajectory Pred Only Delta Psi vs Ground Truth')
-plt.ylabel('Y (m)')
-plt.xlabel('X (m)')
-plt.legend(['Trajectory Ground Truth', 'Trajectory Pred Only Delta Psi'], loc='upper left')
 
 plt.show()
 
-np.savetxt('pred_positions.txt', pred_trajectory, delimiter=' ')
+#gt_trajectory = generate_trajectory_2d(init_l, init_psi, y_delta_l, y_delta_psi)
+#pred_trajectory = generate_trajectory_2d(init_l, init_psi, yhat_delta_l, yhat_delta_psi)
+#pred_trajectory_only_l = generate_trajectory_2d(init_l, init_psi, yhat_delta_l, y_delta_psi)
+#pred_trajectory_only_psi = generate_trajectory_2d(init_l, init_psi, y_delta_l, yhat_delta_psi)
+
+gt_trajectory = generate_trajectory_3d(init_l, init_theta, init_psi, y_delta_l, y_delta_theta, y_delta_psi)
+pred_trajectory = generate_trajectory_3d(init_l, init_theta, init_psi, yhat_delta_l, yhat_delta_theta, yhat_delta_psi)
+
+#gt_trajectory = generate_trajectory_2d(init_l, init_psi, y_delta_l[::20], y_delta_psi[::20])
+#pred_trajectory = generate_trajectory_2d(init_l, init_psi, yhat_delta_l[::20], yhat_delta_psi[::20])
+#pred_trajectory_only_l = generate_trajectory_2d(init_l, init_psi, yhat_delta_l[::20], y_delta_psi[::20])
+#pred_trajectory_only_psi = generate_trajectory_2d(init_l, init_psi, y_delta_l[::20], yhat_delta_psi[::20])
+
+##plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
+#plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
+#plt.title('Trajectory Ground Truth')
+#plt.ylabel('Y (m)')
+#plt.xlabel('X (m)')
+#plt.show()
+
+#fig = plt.figure()
+#ax = fig.gca(projection='3d')
+##ax.plot(gt_trajectory[:, 0], gt_trajectory[:, 1], gt_trajectory[:, 2])
+#ax.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1], gt_trajectory[0:200, 2])
+#ax.set_title('Trajectory Ground Truth');
+#ax.set_xlabel('X (m)')
+#ax.set_ylabel('Y (m)')
+#ax.set_zlabel('Z (m)')
+#plt.show()
+
+#plt.figure()
+##plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
+##plt.plot(pred_trajectory[:, 0], pred_trajectory[:, 1])
+#plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
+#plt.plot(pred_trajectory[0:200, 0], pred_trajectory[0:200, 1])
+#plt.title('Trajectory Pred vs Ground Truth')
+#plt.ylabel('Y (m)')
+#plt.xlabel('X (m)')
+#plt.legend(['Trajectory Ground Truth', 'Trajectory Pred'], loc='upper left')
+
+#plt.figure()
+##plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
+##plt.plot(pred_trajectory_only_l[:, 0], pred_trajectory_only_l[:, 1])
+#plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
+#plt.plot(pred_trajectory_only_l[0:200, 0], pred_trajectory_only_l[0:200, 1])
+#plt.title('Trajectory Pred Only Delta L vs Ground Truth')
+#plt.ylabel('Y (m)')
+#plt.xlabel('X (m)')
+#plt.legend(['Trajectory Ground Truth', 'Trajectory Pred Only Delta Psi'], loc='upper left')
+
+#plt.figure()
+##plt.plot(gt_trajectory[:, 0], gt_trajectory[:, 1])
+##plt.plot(pred_trajectory_only_psi[:, 0], pred_trajectory_only_psi[:, 1])
+#plt.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1])
+#plt.plot(pred_trajectory_only_psi[0:200, 0], pred_trajectory_only_psi[0:200, 1])
+#plt.title('Trajectory Pred Only Delta Psi vs Ground Truth')
+#plt.ylabel('Y (m)')
+#plt.xlabel('X (m)')
+#plt.legend(['Trajectory Ground Truth', 'Trajectory Pred Only Delta Psi'], loc='upper left')
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+#ax.plot(gt_trajectory[:, 0], gt_trajectory[:, 1], gt_trajectory[:, 2])
+#ax.plot(pred_trajectory[:, 0], pred_trajectory[:, 1], pred_trajectory[:, 2])
+ax.plot(gt_trajectory[0:200, 0], gt_trajectory[0:200, 1], gt_trajectory[0:200, 2])
+ax.plot(pred_trajectory[0:200, 0], pred_trajectory[0:200, 1], pred_trajectory[0:200, 2])
+ax.set_title('Trajectory Pred vs Ground Truth');
+ax.set_xlabel('X (m)')
+ax.set_ylabel('Y (m)')
+ax.set_zlabel('Z (m)')
+ax.legend(['Trajectory Ground Truth', 'Trajectory Pred'], loc='upper left')
+
+plt.show()
+
+#np.savetxt('pred_positions.txt', pred_trajectory, delimiter=' ')
