c4b5b6293238c712b0d126a92f661a6689f3b4e7
jpsml <jpsml@cin.ufpe.br>
2019-01-28 15:31:14 +0900

    train with 17 handheld sequences and evaluate with remainin 7 handheld sequences

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,8 @@ from util import *
 
 
 #model_filename = 'bidirectional_lstm_mtl_pred_6D_handheld_all_seqs_1000_epochs.hdf5'
-model_filename = 'bidirectional_lstm_6D_quat_handheld_all_seqs_400_epochs.hdf5'
+#model_filename = 'bidirectional_lstm_6D_quat_handheld_all_seqs_400_epochs.hdf5'
+model_filename = 'bidirectional_lstm_pred.hdf5'
 
 model = load_model(model_filename)
 
@@ -17,25 +18,43 @@ stride = 10
 imu_data_filenames = []
 gt_data_filenames = []
 
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu1.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu2.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu3.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu4.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu5.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu6.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu7.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu8.csv')
-imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu9.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu1.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu2.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu3.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu4.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu5.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu6.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu7.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu8.csv')
+#imu_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/imu9.csv')
 
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi1.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi2.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi3.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi4.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi5.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi6.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi7.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi8.csv')
-gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi9.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi1.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi2.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi3.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi4.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi5.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi6.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi7.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi8.csv')
+#gt_data_filenames.append('Oxford Inertial Tracking Dataset/multi users/user2/syn/vi9.csv')
+
+
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data2/syn/imu1.csv')
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data2/syn/imu2.csv')
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data2/syn/imu3.csv')
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/imu1.csv')
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/imu2.csv')
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/imu3.csv')
+imu_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/imu4.csv')
+
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data2/syn/vi1.csv')
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data2/syn/vi2.csv')
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data2/syn/vi3.csv')
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/vi1.csv')
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/vi2.csv')
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/vi3.csv')
+gt_data_filenames.append('Oxford Inertial Tracking Dataset/handheld/data5/syn/vi4.csv')
+
 
 print('evaluating trajectory rmse (m) using model ' + model_filename)
 
